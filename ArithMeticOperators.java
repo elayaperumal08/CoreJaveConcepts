572b34c56d23353d980e58ade34c0f8b9c360790
elayaperumal <elayaperumalee09@gmail.com>
2020-03-02 17:18:16 +0530

    ArithMeticOperators.java

---
+++ ArithMeticOperators.java
@@ -0,0 +1,31 @@
+package com.elaya.basics;
+
+public class ArithMeticOperators {
+	
+	public static void main (String [] args) {
+		
+		int a, b, c,d,f,g;
+		float e;
+		a=10;
+		b=4;
+		
+		c=a+b;
+		d=a*b;
+		e=a/b;
+		f=a-b;
+		g=a%b;
+		
+		
+		System.out.println("Adding of a and b = " + c);
+		System.out.println("Multiplication of a and b = " + d);
+		System.out.println("Dividing of a and b = " + e);
+		System.out.println("subtraction of a and b = " + f);
+		System.out.println("Post increment = " + a++);
+		System.out.println("Pre increment = " + ++a);
+		System.out.println("Post decrement = " + a--);
+		System.out.println("Pre decrement = " + --a);
+		
+		
+	}
+
+}
