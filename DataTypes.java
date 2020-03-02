d2965ec369cf0227d93cc02f59a86b8b1561acdd
elayaperumal <elayaperumalee09@gmail.com>
2020-03-02 16:05:00 +0530

    DataTypes

---
+++ DataTypes.java
@@ -0,0 +1,36 @@
+package com.elaya.basics;
+
+public class DataTypes {
+	public static void main(String [] args) {
+		int i;      //int holds 4 bytes
+		float f;    //float holds 4 bytes
+		char c;     //char holds 2 bytes
+		long l;    //long holds 8 bytes
+		String s;   
+		double d;   //double holds 8 bytes
+		byte b; //it holds 1 byte
+		short h;  //short holds 2 bytes 
+		boolean o;
+		
+		i=10;
+		s="Elaya";
+		f=10.2f;
+		d=15.5d;
+		h=5;
+		c='E';
+		o=true;
+		
+		System.out.println(i);
+		System.out.println(s);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(h);
+		System.out.println(c);
+		System.out.println(o);
+		
+		
+		
+		
+	}
+
+}
